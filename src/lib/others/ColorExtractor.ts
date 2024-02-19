6460b723a4b55a0846e2110609eced4595a7a28c
akrck02 <aketza.vazquez@gmail.com>
2024-02-19 23:31:12 +0100

    "[wip] hsl seems to be the key... but no quite yet"

--- src/lib/others/ColorExtractor.ts
+++ src/lib/others/ColorExtractor.ts
@@ -1,3 +1,5 @@
+const { abs, min, max, round } = Math;
+
 /**
  * This interface represents the structural color palette
  * It is used to define the colors of the different surfaces and texts
@@ -49,6 +51,10 @@ export class ColorExtractor {
 
         const rgbBaseColor = ColorExtractor.hexToRgb(baseColor);
         console.table(rgbBaseColor);
+
+        //console.log(ColorExtractor.rgbToHsl(rgbBaseColor));
+        
+        // Extract possible colors
 
         const extractedColors = ColorExtractor.extractAll(rgbBaseColor);
         extractedColors.forEach(color => console.log(color));
@@ -244,5 +250,79 @@ export class ColorExtractor {
     private static isLight(color : Color) {
         return color.r + color.g + color.b  >= 127 * 2;
     }
+
+    /**
+     * Converts an HSL color value to RGB. Conversion formula
+     * adapted from https://en.wikipedia.org/wiki/HSL_color_space.
+     * Assumes h, s, and l are contained in the set [0, 1] and
+     * returns r, g, and b in the set [0, 255].
+     *
+     * @param   {number}  h       The hue
+     * @param   {number}  s       The saturation
+     * @param   {number}  l       The lightness
+     * @return  {Array}           The RGB representation
+     */
+    private static hslToRgb(h, s, l) {
+        let r, g, b;
+    
+        if (s === 0) {
+        r = g = b = l; // achromatic
+        } else {
+        const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        const p = 2 * l - q;
+        r = ColorExtractor.hueToRgb(p, q, h + 1/3);
+        g = ColorExtractor.hueToRgb(p, q, h);
+        b = ColorExtractor.hueToRgb(p, q, h - 1/3);
+        }
+    
+        return [round(r * 255), round(g * 255), round(b * 255)];
+    }
+    
+    /**
+     * Converts an RGB color value to HSL. Conversion formula
+     * @param p 
+     * @param q 
+     * @param t 
+     * @returns 
+     */
+    private static hueToRgb(p, q, t) {
+        if (t < 0) t += 1;
+        if (t > 1) t -= 1;
+        if (t < 1/6) return p + (q - p) * 6 * t;
+        if (t < 1/2) return q;
+        if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+        return p;
+    }
+
+    /**
+     * Converts an RGB color value to HSL. Conversion formula
+     * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+     * Assumes r, g, and b are contained in the set [0, 255] and
+     * returns h, s, and l in the set [0, 1].
+     *
+     * @param   {number}  r       The red color value
+     * @param   {number}  g       The green color value
+     * @param   {number}  b       The blue color value
+     * @return  {Array}           The HSL representation
+     */
+    private static rgbToHsl(color : Color) {
+        (color.r /= 255), (color.g /= 255), (color.b /= 255);
+        const vmax = max(color.r, color.g, color.b), vmin = min(color.r, color.g, color.b);
+        let h, s, l = (vmax + vmin) / 2;
+    
+        if (vmax === vmin) {
+        return [0, 0, l]; // achromatic
+        }
+    
+        const d = vmax - vmin;
+        s = l > 0.5 ? d / (2 - vmax - vmin) : d / (vmax + vmin);
+        if (vmax === color.r) h = (color.g - color.b) / d + (color.g < color.b ? 6 : 0);
+        if (vmax === color.g) h = (color.b - color.r) / d + 2;
+        if (vmax === color.b) h = (color.r - color.g) / d + 4;
+        h /= 6;
+    
+        return [h, s, l];
+    }
+  
 
 } 
